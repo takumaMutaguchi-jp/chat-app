a9e6205b559ca1bd6904df9c8aea4de791104813
takumaMutaguchi-jp <mutaguchitakuma@gmail.com>
2020-12-18 08:49:43 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_many :rooms
+  has_many :rooms, through: :room_users
   validates :name, presence: true
 end
